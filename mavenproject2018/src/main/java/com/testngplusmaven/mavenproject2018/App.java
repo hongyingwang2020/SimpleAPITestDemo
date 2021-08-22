30db8888fcf17d8b702d89eb39f325d82a8ac6ff
wanghongying2020@gmail.com <me@wanghongying2020@gmail.com>
2021-08-22 20:03:26 +0100

    add maven project with seleniu and testNG

---
+++ mavenproject2018/src/main/java/com/testngplusmaven/mavenproject2018/App.java
@@ -0,0 +1,13 @@
+package com.testngplusmaven.mavenproject2018;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
